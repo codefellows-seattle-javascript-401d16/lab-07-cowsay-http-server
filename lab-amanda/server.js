4203659916ea804429fcf420597c50a276f83ff9
Amanda Koster <amandakoster@gmail.com>
2017-06-20 18:01:39 -0700

    GETs are done wworking on PUTs

--- lab-amanda/server.js
+++ lab-amanda/server.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const http = require('http');
-const url = requre ('url');
+const url = require ('url');
 const querystring = require ('querystring');
-
+const cowsay = require('cowsay');
 const bodyParse = (req, callback) => {
   if(req.method === 'POST' || req.method === 'PUT') {
     let body = ' ';
-    req. on('data', (buf) => {
+    req.on('data', (buf) => {
       body += buf.toString();
     });
     req.on('end', () => callback(null, body));
-    req.on('error', (err) => callback (err));
+    req.on('error', (err) => callback(err));
   } else {
-    callback(null, {});
+    callback(null, '{}' );
   }
 };
 
@@ -28,7 +28,6 @@ const server = http.createServer((req, res) => {
       res.end();
       return;
     }
-
     try {
       req.body = JSON.parse(body);
     } catch (err) {
@@ -36,31 +35,70 @@ const server = http.createServer((req, res) => {
       res.end();
       return;
     }
-if(req.method === 'GET' && req.url.pathname === '/time'){
-  res.writeHead(200, {
-    'Content-Type' : 'application/json',
-});
 
-res.write(JSON.stringify){
-  now: Date.now(),
-  date: new Date(),
-  }));
-res.end();
-return
-}
+    if(req.method === 'GET' && req.url.pathname === '/time'){
+      res.writeHead(200, {
+        'Content-Type' : 'application/json',
+      });
 
-if(req.method === 'POST' && req.url.pathname === '/echo'){
-  console.log('coooool');
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
+      res.write(JSON.stringify( {
+        now: Date.now(),
+        date: new Date(),
+      }));
+      res.end();
+      return;
+    }
+
+    if(req.url.pathname === '/'){
+      res.writeHead(200, {
+        'Content-Type' : 'text/plain',
+      });
+      res.write('hello world');
+      res.end();
+      return;
+    }
+
+    if (req.method === 'GET' && req.url.pathname === '/cowsay'){
+      res.writeHead(200, {
+        'Content-Type' : 'text/plain',
+      });
+      res.write((cowsay.say({
+        text : 'I\'m a moooodule',
+        e : 'oO',
+        T : 'U ',
+      })));
+      res.end();
+      return;
+    } else {
+      res.writeHead(500);
+      res.write({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'});
+      res.end();
+      return;
+    }
+
+
+  // if(req.method === 'POST' && req.url.pathname === '/echo'){
+  //   // console.log('coooool');
+  //   res.writeHead(200, {
+  //     'Content-Type': 'application/json',
+  //   });
+  //   res.write(JSON.stringify(' '));
+  //   res.end();
+  //   return;
+  // }
+
+    if(req.method === 'POST' && req.url.pathname === '/cowsay'){
+      res.writeHead(200, {
+        'Content-Type' : 'text/plain',
+      });
+      res.write('tedt');
+      res.end();
+      return;
+    }
+
+    res.writeHead(444);
+    res.end();
   });
-  res.write(JSON.stringify(req.body));
-  res.end()
-  return;
-}
-res.writeHead(444);
-res.end();
-});
 });
 
 server.listen(3000, () => {
