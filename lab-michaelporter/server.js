993ab1abe283563f33c4a65349e16124970104e4
mikeporter <porter.michael.james@gmail.com>
2017-06-20 19:58:00 -0700

    handled /, /cowsay and wrote .txts to test them

--- lab-michaelporter/server.js
+++ lab-michaelporter/server.js
@@ -2,7 +2,8 @@
 
 const http = require('http');
 const url = require('url');
-const querystring = require('quertystring');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
 
 const bodyParse = (req, callback) => {
   if (req.method === 'POST' || req.method === 'PUT') {
@@ -20,28 +21,55 @@ const bodyParse = (req, callback) => {
 const server = http.createServer((req, res) => {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
-  bodyParse(req, (err, body) => {
+  bodyParse(req, (err, concatedBody) => {
     if (err) {
-      res.writeHead(500);
-      res.end();
-      return;
-    }
-
-    try {
-      req.body = JSON.parse(body);
-    } catch (err) {
-      res.writeHead(400);
-      res.end();
-      return;
-    }
-
-    if (req.method === 'GET' && req.url.pathname === '/time') {
-      res.writeHead(200, {
-        'Content-Type' : 'application/json',
+      res.writeHead(500, {
+        'Content-Type': 'text/plain',
       });
-      res.write(JSON.stringify({
-        
-      }));
+      res.write('Internal Server Error');
+      res.end();
+      return;
+    }
+
+    if (req.url.pathname === '/') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      res.write('hello world');
+      res.end();
+      return;
+    }
+
+    if (req.method === 'GET' && req.url.pathname === '/cowsay') {
+      if (req.url.query['text']) {
+        res.writeHead(200, {
+          'Content-type': 'text/plain',
+        });
+        res.write(cowsay.say({text: req.url.query['text']}));
+        res.end();
+        return;
+      }
+      res.writeHead(400, {
+        'Content-type': 'text/plain',
+      });
+      res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+      res.end();
+      return;
+    }
+
+    if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+      if (req.url.quert['text']) {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain',
+        });
+        res.write(cowsay.say({text: req.url.query['text']}));
+        res.end();
+        return;
+      }
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
       res.end();
       return;
     }
