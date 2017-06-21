fabaa8c3e881d558b1950b9f44bcca2ac4153083
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-20 21:04:00 -0700

    refactor index.js

--- lab-michaelstuart/index.js
+++ lab-michaelstuart/index.js
@@ -4,7 +4,21 @@ const querystring = require('querystring');
 const cowsay = require('cowsay');
 const bodyParse = require('./lib/body-parse');
 
-const server = module.exports = http.createServer((req, res) => {
+const makeCowsay = (req, res) => {
+  let text = req.method === 'GET' ? req.url.query.text : req.body.text;
+  let code = 200;
+
+  if (!text) {
+    code = 400;
+    text = 'bad request\ntry: localhost:3000/cowsay?text=howdy';
+  }
+
+  res.writeHead(code, { 'Content-Type' : 'text/plain' });
+  res.write(cowsay.say({ text }));
+  res.end();
+};
+
+module.exports = http.createServer((req, res) => {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
@@ -13,9 +27,12 @@ const server = module.exports = http.createServer((req, res) => {
       res.writeHead(500);
       res.end();
       return;
-    } try {
+    }
+
+    try {
       req.body = JSON.parse(body);
-    } catch (err) {
+    }
+    catch (err) {
       res.writeHead(400);
       res.end();
       return;
@@ -28,29 +45,8 @@ const server = module.exports = http.createServer((req, res) => {
       return;
     }
 
-    if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-      const text = req.url.query.text;
-      if (text) {
-        res.writeHead(200, { 'Content-Type' : 'text/plain' });
-        res.write(cowsay.say({ text }));
-      } else {
-        res.writeHead(400, { 'Content-Type' : 'text/plain' });
-        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
-      }
-      res.end();
-      return;
-    }
-
-    if (req.method === 'POST' && req.url.pathname === '/cowsay') {
-      const text = req.body.text;
-      if (text) {
-        res.writeHead(200, { 'Content-Type' : 'text/plain' });
-        res.write(cowsay.say({ text }));
-      } else {
-        res.writeHead(400, { 'Content-Type' : 'text/plain' });
-        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
-      }
-      res.end();
+    if (req.url.pathname === '/cowsay') {
+      makeCowsay(req, res);
       return;
     }
 
@@ -58,8 +54,4 @@ const server = module.exports = http.createServer((req, res) => {
     res.end();
   });
 
-});
-
-server.listen(8080, () => {
-  console.log('server up on port 8080');
-});
+}).listen(8080, () => console.log('server running on port 8080'));
