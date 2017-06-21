9a125b987ebf1e74b665dc55f658c3e8c3efc11c
jeffsford <jeff@jeffford.com>
2017-06-20 17:51:55 -0700

    fixed linter errors

--- lab-jeff/index.js
+++ lab-jeff/index.js
@@ -5,26 +5,23 @@ const url = require('url');
 const querystring = require('querystring');
 const cowsay = require('cowsay');
 
-
 const bodyParse = (req, callback) => {
   if(req.method === 'POST' || req.method === 'PUT'){
-    let body = ''
+    let body = '';
     req.on('data', (buf) => {
       body += buf.toString();
     });
     req.on('end', () => callback(null, body));
-    req.on('error', (err) => callback(err))
+    req.on('error', (err) => callback(err));
   } else {
-    callback(null, '{}')
+    callback(null, '{}');
   }
-}
+};
 
 const server = http.createServer((req, res) => {
   req.url = url.parse(req.url);
-  req.url.query = querystring.parse(req.url.query)
+  req.url.query = querystring.parse(req.url.query);
   console.log(req.url.query.text);
-  // console.log('req.url', req.url);
-  // console.log('req.method', req.method);
   bodyParse(req, (err, body) => {
     if(err) {
 
@@ -34,7 +31,7 @@ const server = http.createServer((req, res) => {
     }
 
     try {
-      req.body = JSON.parse(body)
+      req.body = JSON.parse(body);
     } catch (err) {
 
       res.writeHead(400);
@@ -48,11 +45,11 @@ const server = http.createServer((req, res) => {
       });
       res.write('hello world');
       res.end();
-      return
+      return;
     }
 
     if(req.method === 'GET' && req.url.pathname === '/cowsay'){
-      if(req.url.query.text === undefined){
+      if(req.url.query.text === null){
         res.writeHead(400);
         res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
         res.end();
@@ -67,9 +64,8 @@ const server = http.createServer((req, res) => {
       }
     }
 
-    // if the pathname is /echo and a POST req send back their body as json
     if(req.method === 'POST' && req.url.pathname === '/cowsay'){
-      if(req.url.query.text === undefined){
+      if(req.url.query.text === null){
         res.writeHead(400);
         res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
         res.end();
@@ -83,9 +79,7 @@ const server = http.createServer((req, res) => {
         return;
       }
     }
-
   });
-
 });
 
 server.listen(3000, () => {
