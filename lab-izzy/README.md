4562a3df4b47773a1328c6399d246f0da6276845
Izabella Baer <izabellabaer@gmail.com>
2017-06-20 17:35:46 -0700

    added get and post and used body parser and everything works, cow says moooodule!

--- lab-izzy/README.md
+++ lab-izzy/README.md
@@ -1,6 +1,11 @@
-# Cowsay HTTP server
+# What does the COW say? MOOOOODule!
+\n  
+# For this project I created an HTTP Server using the http module and created a parse body module to be used for all POST requests. I created conditions for / and /cowsay.
 \n
-# Created a HTTP Server using the http module.
+# GET request
 \n
-# Created a parse body module that is used for all POST request
+# I created an HTTP GET request that had an if statement to check if the query string was set, we responded with a 202 and specific message, if it was not set we responded with a 400 and a specific message.
 \n
+# POST request
+\n
+# I created an HTTP POST request which utilized a parse body module to parse the body of the request. I checked the body to make sure it was set in the body, if it was I responded with a status code of 200 and a specific message. If it was not set I responded with a status code of 400 and a specific message.
--- lab-izzy/server.js
+++ lab-izzy/server.js
@@ -3,78 +3,49 @@
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
+const cowsay = require('cowsay');
+const bodyParser = require('./lib/body-parse');
 
-// callback should be (err, body) => undefined
-const bodyParse = (req, callback) => {
-  if(req.method === 'POST' || req.method === 'PUT') {
-    let body = '';
-    req.on('data', (buf) => {
-      body += buf.toString();
-    });
-    req.on('end', () => callback(null, body));
-    req.on('error', (err) => callback(err));
-  } else {
-    callback(null, '{}');
-  }
-};
-
-const server = http.createServer((req, res) => {
+const server = module.exports = http.createServer((req, res) => {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
-  console.log('req.url', req.url);
-  console.log('req.method', req.method);
-  bodyParse(req, (err, body) => {
-    if(err) {
 
-      // respond to the user with a 500 server Error
-      res.writeHead(500);
-      res.end();
-      return;
-    }
+  if(req.url.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.write('hello world');
+    res.end();
+    return;
+  }
 
-
-    // parse the body as json
-    try {
-      req.body = JSON.parse(body);
-    } catch (err) {
-
-      //if there was mal formatted JSON we send back a 400 bad request
-      res.writeHead(400);
-      res.end();
-      return;
-    }
-
-    // respond with a 200 status code and yay
-    // if the pathname is /time and a GET req send back the date
-    if(req.method === 'GET' && req.url.pathname === '/time') {
-      res.writeHead(200, {
-        'Content-Type' : 'text/plain',
-      });
-      res.write(JSON.stringify({
-        now: Date.now(),
-        data: new Date(),
+  if(req.method === 'GET' && req.url.pathname === '/cowsay') {
+    console.log('REQ BODY:', req.url);
+    if(req.url.query.text){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(cowsay.say({
+        text: JSON.stringify(req.url.query.text),
       }));
       res.end();
       return;
-    }
-
-    // if the pathname is /echo and a POST req send back their body as JSON
-    if(req.method === 'POST' && req.url.pathname === '/echo') {
-      console.log('sweet dude');
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      res.write(JSON.stringify(req.body));
+    } else {
+      res.writeHead(400, {'Content-Type':'text/plain'});
+      res.message(cowsay.say({text: 'bad request\ntry localhost:3000/cowsay?text=howdy'}));
       res.end();
       return;
     }
+  }
 
-    // otherwise 404
-    res.writeHead(404);
-    res.end();
+  if(req.method === 'POST' && req.url.pathname === '/cowsay') {
+    bodyParser(req, (err, body) => {
+      if(err) {console.error(err)
+      res.writeHead(400, {})
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(cowsay.say({text: body.text}));
+        res.end();
+        return;
+      }
+    });
+
+  server.listen(3000, () => {
+    console.log('servin up some peaky blinderz on port 3000');
   });
-});
-
-server.listen(3000, () => {
-  console.log('servin up some peaky blinderz on port 3000');
-});
