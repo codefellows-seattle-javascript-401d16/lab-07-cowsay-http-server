6603716862e8100baec5fe974b5a449ba003160f
Saul Greene <spdysg@gmail.com>
2017-07-06 00:16:53 -0700

    ready for resubmit

--- lab-saul/index.js
+++ lab-saul/index.js
@@ -8,20 +8,20 @@ const cowsay = require('cowsay');
 //move to lib
 const bodyParse = (req, callback) => {
   if(req.method === 'POST' || req.method === 'PUT'){
-    let body = ''
+    let body = '';
     req.on('data', (buf) => {
       body += buf.toString();
     });
     req.on('end', () => callback(null, body));
-    req.on('error', (err) => callback(err))
+    req.on('error', (err) => callback(err));
   } else {
-    callback(null, '{}')
+    callback(null, '{}');
   }
-}
+};
 
 const server = http.createServer((req, res) => {
   req.url = url.parse(req.url);
-  req.url.query = querystring.parse(req.url.query)
+  req.url.query = querystring.parse(req.url.query);
   console.log('req.url', req.url);
   console.log('req.method', req.method);
   bodyParse(req, (err, body) => {
@@ -34,7 +34,7 @@ const server = http.createServer((req, res) => {
 
     // parse the body as json
     try {
-      req.body = JSON.parse(body)
+      req.body = JSON.parse(body);
     } catch (err) {
       // if there was mal formated JSON we send back a 400 bad request
       res.writeHead(400);
@@ -54,7 +54,7 @@ const server = http.createServer((req, res) => {
         date: new Date(),
       }));
       res.end();
-      return
+      return;
     }
 
     // if the pathname is /echo and a POST req send back their body as json
@@ -64,13 +64,13 @@ const server = http.createServer((req, res) => {
         'Content-Type': 'application/json',
       });
       res.write(JSON.stringify(req.body));
-      res.end()
+      res.end();
       return;
     }
     /////////////////////////////////////////////////////////
     if(req.method === 'POST') {
 
-    if(req.url.pathname === '/cowsay') {
+      if(req.url.pathname === '/cowsay') {
       // bodyParser(req, function(err) {``
         // if(err) console.error(err);
         let message = cowsay.say({text: req.body.text});
@@ -79,39 +79,37 @@ const server = http.createServer((req, res) => {
         res.write(message);
         res.end();
       // });
-    } else {
-      let message = cowsay.say(
-        {text: 'bad request\ntry localhost:3000/cowsay with a proper body'}
-      );
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write(message);
-      res.end();
-    }
-  }
-
-  if(req.method === 'GET') {
-    if(req.url.pathname === '/cowsay') {
-      if(req.url.query.text){
-        let message = cowsay.say({text: req.url.query.text});
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+      } else {
+        let message = cowsay.say(
+          {text: 'bad request\ntry localhost:3000/cowsay with a proper body'}
+        );
+        res.writeHead(400, {'Content-Type': 'text/plain'});
         res.write(message);
         res.end();
-      }else{
-        res.writeHead(400);
-        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+      }
+    }
+
+    if(req.method === 'GET') {
+      if(req.url.pathname === '/cowsay') {
+        if(req.url.query.text){
+          let message = cowsay.say({text: req.url.query.text});
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.write(message);
+          res.end();
+        }else{
+          res.writeHead(400);
+          res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+          res.end();
+        }
+      }else {
+        let message = cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}
+        );
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.write(message);
         res.end();
       }
-    }else {
-      let message = cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}
-      );
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write(message);
-      res.end();
     }
-    // if('/') //return 'hello world'
-  }
-  //////////////////////////////////////////////////////////
-    // otherwise 404
+
     res.writeHead(444);
     res.end();
   });
